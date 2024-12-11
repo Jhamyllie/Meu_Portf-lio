2ef5616db197e75bc7704b4ca3b49e834754f9d2
Jhamyllie <milli.santana2017@gmail.com>
2024-12-11 16:26:29 -0300

    fix: Parcialmente finalisando

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,12 @@ const menuHamburger = document.querySelector('.menu-hamburger');
 const nav = document.querySelector('.nav');
 
 menuHamburger.addEventListener('click', () => {
-    menuHamburger.classList.toggle('active'); // Adiciona a classe de animação ao hambúrguer
-    nav.classList.toggle('active'); // Mostra ou esconde o menu de navegação
+    menuHamburger.classList.toggle('active'); 
+    nav.classList.toggle('active'); 
+});
+
+document.getElementById('contact-form').addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    document.getElementById('contact-form').reset();
+    alert('Mensagem enviada com sucesso!');
 });
