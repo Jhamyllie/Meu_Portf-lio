9a7a66ced59494b5b6e93c1701d490dcc4517d6d
Jhamyllie <milli.santana2017@gmail.com>
2024-12-11 16:43:40 -0300

    fix: Adiciona comentários no código.

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,14 @@
+// constantes usadas para pegarem os elementos html a serem aplicadas funcionalidades.
 const menuHamburger = document.querySelector('.menu-hamburger');
 const nav = document.querySelector('.nav');
 
+// executa animação do menu hambútger
 menuHamburger.addEventListener('click', () => {
     menuHamburger.classList.toggle('active'); 
     nav.classList.toggle('active'); 
 });
 
+// Limpa o formulário após clicar no botão de enviar
 document.getElementById('contact-form').addEventListener('submit', (event) => {
     event.preventDefault(); 
     document.getElementById('contact-form').reset();
